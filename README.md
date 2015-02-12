f00fa74440316795e1b339e1a866125b7d94c74e
cnichols <mellowdious@mail.com>
2015-02-11 16:58:20 -0800

    working webcam retrieval, incl geolocation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#Weather App
+#Weather Webcam App
+
+Displays area webcams based on given zip code, or based on user's current position.
--- client/main.js
+++ client/main.js
@@ -1,1 +1,44 @@
-console.log('hello, world');
+'use strict';
+$(document).ready(init);
+
+var apiURL = 'http://api.wunderground.com/api/e0e26c847eb5b7b7/';
+
+function init() {
+  $('#get-cams').click(clickGetCams);
+  $('#my-cams').click(clickMyCams);
+}
+
+function clickMyCams() {
+  var options = {enableHighAccuracy: true, timeout: 5000, maximumAge: 0};
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function success(pos) {
+  console.log('You are here:', pos);
+  var lat = pos.coords.latitude;
+  var lon = pos.coords.longitude;
+  var url = apiURL + 'webcams/q/' + lat + ',' + lon + '.json';
+  paint(url);
+}
+
+function error(err) {
+  console.log('Could not find position', err);
+}
+
+function clickGetCams() {
+  var zipCode = $('#zip-code').val();
+  var url = apiURL + 'webcams/q/' + zipCode + '.json';
+  paint(url);
+}
+
+function paint(url) {
+  $.getJSON(url, function(response) {
+    $('#images').empty();
+    response.webcams.forEach(function(cam) {
+      var $img = $('<div>');
+      $img.addClass('image');
+      $img.css('background-image', 'url("' + cam.CURRENTIMAGEURL + '")');
+      $('#images').append($img);
+    });
+  });
+}
